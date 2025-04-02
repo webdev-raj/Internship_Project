a29938878e07397b3bf22cd14350f60003801b9b
webdev-raj <codewithsapnap@gmail.com>
2025-04-02 18:50:40 +0530

    adding loader

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 import { gsap } from 'gsap'
 const allImg = document.querySelectorAll(".images")
+let tl = gsap.timeline()
+
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    loader()
     loadingAnimation()
     imageAndTextAnimation()
     mouseAnimationOnImg()
 })
+
+const loader = ()=>{
+    let texth1 = document.querySelector("#loader h1")
+    let texth1innertext = texth1.textContent
+    let splitedText=texth1innertext.split("")
+    let clutter = ""
+    splitedText.forEach((letters)=>{
+        clutter += `<span class="inline-block">${letters}</span>`
+    })
+    texth1.innerHTML = clutter
+
+    tl.from("#loader h1 span",{
+        y:40,
+        duration:0.9,
+        stagger:0.3,
+        opacity:0
+    })
+    tl.to("#loader",{
+        y:"-100%",
+        opacity:0,
+        duration:0.9
+    })
+}
 const loadingAnimation = () => {
-    var tl = gsap.timeline();
     tl.from(".textContainer h1", {
         y: "100%",
-        duration: 0.5,
+        duration: 0.9,
         stagger: 0.3,
         ease: "power2.out",
     })
         .from(".images", {
             scale: 0,
             transform: "rotate(10deg)",
-            duration: 0.5,
-            stagger: 0.4,
+            duration: 0.9,
+            stagger: 0.3,
             ease: "power2.out",
         })
 }
@@ -31,13 +56,13 @@ const imageAndTextAnimation = () => {
             elementDiv.style.backgroundImage = `url("/assets/images/img${indx}.png")`
             gsap.to(elementDiv, {
                 zIndex: 1,
-                duration: 0.5,
+                duration: 0.9,
                 ease: "power2.out",
             }, "same")
             gsap.to(".textContainer h1", {
                 color: "transparent",
                 WebkitTextStroke: "0.5px #4e4d4d",
-                duration: 0.5,
+                duration: 0.9,
                 ease: "power2.out",
             }, "same")
         })
@@ -47,12 +72,12 @@ const imageAndTextAnimation = () => {
             elementDiv.style.backgroundImage = null
             gsap.to(elementDiv, {
                 zIndex: 0,
-                duration: 0.5,
+                duration: 0.9,
                 ease: "power2.out",
             }, "both")
             gsap.to(".textContainer h1", {
                 color: "white",
-                duration: 0.5,
+                duration: 0.9,
                 ease: "power2.out",
             }, "both");
         })
